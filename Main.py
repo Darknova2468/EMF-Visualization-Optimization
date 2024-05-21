d2a2ace85cb1bdd50787b670494bb994be30e8b3
Darknova2468 <112370026+Darknova2468@users.noreply.github.com>
2024-05-20 18:32:45 -0600

    comments

--- Main.py
+++ Main.py
@@ -1,10 +1,6 @@
 #libraries
-import matplotlib.pyplot as plt # type: ignore # type: ignores
-import numpy as np # type: ignore
-import math as m
 import Fields as f
 import Plots as p
-
 
 #initializes the "coil" an array of wire elements
 myCoil = f.Coil()

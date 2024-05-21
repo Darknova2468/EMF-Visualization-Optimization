dda4cd1642147e91f836071a07ede6fa9c72b1da
Darknova2468 <112370026+Darknova2468@users.noreply.github.com>
2024-05-20 18:31:37 -0600

    refactoring

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt # type: ignore # type: ignores
 import numpy as np # type: ignore
 import math as m
 import Fields as f
+import Plots as p
 
 
 #initializes the "coil" an array of wire elements
@@ -12,29 +13,6 @@ myCoil = f.Coil()
 myCoil.addElement(f.Wire(10, [0,-0.5, 0.0], [0,0,0],  1))
 myCoil.addElement(f.Wire(10, [0,0.5, 0.0], [0,0,0], -1))
 
-# Fixed x value for the yz-plane
-x_fixed = 0
+myPlot = p.plot(myCoil, 100)
 
-# Set the grid width and generate the grid points for the yz-plane
-w = 2
-Z, Y = np.mgrid[-w:w:500j, -w:w:500j]
-
-# Initialize arrays for J and K components of the vector field
-J = np.zeros_like(Y)
-K = np.zeros_like(Z)
-
-# Apply the vector field function to each point in the yz-plane
-for i in range(Y.shape[0]):
-    for j in range(Y.shape[1]):
-        _, J[i, j], K[i, j] = myCoil.get([x_fixed, Y[i, j], Z[i, j]])
-
-# Calculate the magnitude of the vector field
-magnitude = np.sqrt(J**2 + K**2)
-
-#plots Vector Field
-plt.streamplot(Y, Z, J, K, color=magnitude, linewidth=2, cmap='viridis')
-plt.colorbar(label='Speed')
-plt.title('YZ Plane EMF Field')
-plt.xlabel('Y')
-plt.ylabel('Z')
-plt.show()
+myPlot.showPlot("YZ", [[-3, 3],[-3,3]], 0)
